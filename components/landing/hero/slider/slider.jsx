eb799dc14bf0a2f31e3b14ba5939256de8c35115
hammadkhanwazir <insafstudentpti@gmail.com>
2023-08-20 12:19:45 +0500

    ok

--- components/landing/hero/slider/slider.jsx
+++ components/landing/hero/slider/slider.jsx
@@ -10,7 +10,7 @@ import "swiper/css/pagination";
 import "./styles.css";
 
 // import required modules
-import { EffectCoverflow, Pagination } from "swiper/modules";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 
 export default function Slider() {
   return (
@@ -20,6 +20,10 @@ export default function Slider() {
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={"auto"}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
@@ -28,47 +32,47 @@ export default function Slider() {
           slideShadows: true,
         }}
         pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
         className="mySwiper"
       >
         <SwiperSlide>
           <div className="flex flex-col gap-4">
-            <img src="/images/team/image.png" alt="" />
+            <img src="/images/team/Image.png" alt="" />
             <span className="span_name">Robert C. Simmons</span>
             <span className="consultant">Business Consultant</span>
           </div>{" "}
         </SwiperSlide>
         <SwiperSlide>
           <div className="flex flex-col gap-4">
-            <img src="/images/team/image 2.png" alt="" />
+            <img src="/images/team/Image 2.png" alt="" />
             <span className="span_name">Christopher L. Wagner</span>
             <span className="consultant">Senior Manager</span>
           </div>{" "}
         </SwiperSlide>
         <SwiperSlide>
           <div className="flex flex-col gap-4">
-            <img src="/images/team/image 3.png" alt="" />
+            <img src="/images/team/Image 3.png" alt="" />
             <span className="span_name">Lawrence C. Dickerson</span>
             <span className="consultant">Financial Consultant</span>
           </div>{" "}
         </SwiperSlide>
         <SwiperSlide>
           <div className="md:flex  flex-col gap-4">
-            <img src="/images/team/image.png" alt="" />
+            <img src="/images/team/Image.png" alt="" />
             <span className="span_name">Robert C. Simmons</span>
             <span className="consultant">Business Consultant</span>
           </div>{" "}
         </SwiperSlide>
         <SwiperSlide>
           <div className="md:flex flex-col gap-4">
-            <img src="/images/team/image 2.png" alt="" />
+            <img src="/images/team/Image 2.png" alt="" />
             <span className="span_name">Christopher L. Wagner</span>
             <span className="consultant">Senior Manager</span>
           </div>{" "}
         </SwiperSlide>
         <SwiperSlide>
           <div className="flex flex-col gap-4">
-            <img src="/images/team/image 3.png" alt="" />
+            <img src="/images/team/Image 3.png" alt="" />
             <span className="span_name">Lawrence C. Dickerson</span>
             <span className="consultant">Financial Consultant</span>
           </div>{" "}
